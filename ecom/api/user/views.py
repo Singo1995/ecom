332048173d0c28fec1a750dbbdaf4abead59c4d8
ArjunSingh <surendersingh1995nov@gmail.com>
2020-09-21 09:15:56 +0530

    Adding all the changes

--- ecom/api/user/views.py
+++ ecom/api/user/views.py
@@ -8,49 +8,52 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import login,logout
 import re
 import random
+import time
 # Create your views here.
 
 
 def generate_session_token(length=10):
-    ''.join(random.SystemRandom().choice([chr(i) for i in range(97, 123)]+[str(i) for i in range(10)]) for _ in range(10))
+    return ''.join(random.SystemRandom().choice([chr(i) for i in range(97, 123)]+[str(i) for i in range(10)]) for _ in range(length))
 
 @csrf_exempt
 def signin(request):
     if not request.method == 'POST':
-        return JsonResponse({'error':'Send a post request with valid parameters'})
-    
+        return JsonResponse({'error': 'Send a post request with valid parameter only'})
+
     username = request.POST['email']
     password = request.POST['password']
-    print("username",username)
-    if not re.match('[\w\.-]+@[\w\.-]+\.\w{2,4}',username):
-        return JsonResponse({'error':'Enter a valid email'})
 
-    if len(password)< 3:
-        return JsonResponse({'error':'Password needs to be at least 3 character length'})
+    if not re.match("^[\w\.\+\-]+\@[\w]+\.[a-z]{2,3}$", username):
+        return JsonResponse({'error': 'Enter a valid email'})
+
+    if len(password) < 3:
+        return JsonResponse({'error': 'Password needs to be at least 3 characters'})
 
     UserModel = get_user_model()
 
     try:
         user = UserModel.objects.get(email=username)
+
         if user.check_password(password):
             usr_dict = UserModel.objects.filter(email=username).values().first()
+
             usr_dict.pop('password')
 
             if user.session_token != "0":
                 user.session_token = "0"
                 user.save()
-                return JsonResponse({'error':'Previous Session Exists'})
-                
+                return JsonResponse({'error': 'Previous session exists'})
+
             token = generate_session_token()
             user.session_token = token
             user.save()
-            login(request, user)
-
+            #login(request, user)
+            return JsonResponse({'token': token, 'user': usr_dict})
         else:
-            return JsonResponse({'error':'Invalid password'})
+            return JsonResponse({'error': 'Invalid Password'})
 
     except UserModel.DoesNotExist:
-        return JsonResponse({'error':'Invalid Email'})
+        return JsonResponse({'error': 'Invalid Email'})
 
 
 def signout(request,id):
